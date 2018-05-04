7e343f5e0c46d25c46f61d72c58813a6598be354
Byron Igoe <byronigoe@gmail.com>
2018-05-04 07:27:31 -0400

    Bump version

--- angular-text-parse.js
+++ angular-text-parse.js
@@ -1,6 +1,6 @@
 /**
  * Generic text parsing utility to create JSON from structured TXT
- * @version v0.1.0 - 2016-09-01 * @link https://github.com/byronigoe/angular-text-parse
+ * @version v1.1.0 - 2018-05-04 * @link https://github.com/byronigoe/angular-text-parse
  * @author Byron Igoe <byronigoe@gmail.com>
  * @license GPL 3.0 License, https://www.gnu.org/licenses/gpl-3.0.en.html
  */
